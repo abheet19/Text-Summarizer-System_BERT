28b2d4f539245bf0f6335d8b53ec1ff2ede150af
abheet19 <60404707+abheet19@users.noreply.github.com>
2021-01-10 00:54:23 +0530

    inital commit
    Signed-off-by: abheet19 <60404707+abheet19@users.noreply.github.com>

---
+++ app.py
@@ -0,0 +1,139 @@
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from os import path
+import json
+from newspaper import fulltext
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.pyplot as plt
+import re
+from summarizer import Summarizer
+import PyPDF2
+import docx
+import pathlib
+from urllib.parse import urlparse
+
+app = Flask(__name__)
+
+#--------------------helper functions for inner processing--------------------#
+
+
+def clean_and_process(text):
+    # cleaning the text
+
+    clean_abstract = []
+    clean_abstract = re.sub(r'\d',' ',text)
+    clean_abstract = re.sub(r'\W', ' ',clean_abstract)    
+    clean_abstract = re.sub(r'\s+', ' ',text)
+    text = re.sub(r'\[[0-9]*\]',' ',text)
+
+    # saving the wordcloud for pictorial  representation
+
+    wordcloud = WordCloud(max_font_size=100, max_words=100,
+                          background_color="white").generate(str(text))
+    plt.figure()
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.savefig('wordcloud.png')
+
+    return text
+
+
+def BERT(cleaned_text):
+    
+    model = Summarizer()
+    result = model(cleaned_text, min_length=30)
+    full = "".join(result)
+    return full
+
+
+def url_validator(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc, result.path])
+    except:
+        return False
+
+#-----------------------------Routing methods----------------------------#
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+    return render_template('index.html')
+
+
+@app.route('/PDF', methods=['GET', 'POST'])
+def PDF():
+    return render_template('PDF.html')
+
+
+@app.route('/PDF_result', methods=['GET', 'POST'])
+def PDF_result():
+    if request.method == 'POST':
+        if(not request.files['name']):
+            return "<h1> Please for the sake of the lord , provide a file</h1>"
+        else:
+            f = request.files['name']
+            f.save(f.filename)
+
+            # for checking the file format
+            suffix = pathlib.Path(f.filename).suffix
+
+             # for PDF file
+            if suffix == ".pdf": 
+                pdf = open(f.filename, 'rb')
+
+                pdfReader = PyPDF2.PdfFileReader(pdf)
+                num = pdfReader.numPages
+                # indexing of pages is from (0 to num1)
+                pageobj = pdfReader.pages
+                text = ""
+                for i in range(num):  # iterating over every page
+                    text += pageobj[i].extractText()  # we have the raw text in the text variable
+                pdf.close()
+                cleaned_text = clean_and_process(text)
+
+                # summarizer BERT model
+
+                summary=BERT(cleaned_text)
+
+
+            elif suffix == ".docx":  # for WORD file
+                pass
+
+            elif suffix == ".txt":  # for TXT file
+                pass
+            else:
+                return "<h1> Error! Please upload  PDF/WORD/TXT file</h1>"
+
+    
+    return render_template('PDF_result.html',summary=summary)
+
+
+@app.route('/RAW', methods=['GET', 'POST'])
+def RAW():
+    return render_template('RAW.html')
+
+
+@app.route('/RAW_result', methods=['GET', 'POST'])
+def RAW_result():
+    if request.method == 'POST':
+
+        url=request.form.get('name')  #get the URL from the user
+        
+
+        if url_validator(url) is True:  #url validation
+            pass
+
+        else:
+            return "<h1>Error! Please upload a correct URL </h1>"
+
+    
+
+
+
+    return render_template('RAW_result.html')
+
+#-------------------Routing ends----------------------------#
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
